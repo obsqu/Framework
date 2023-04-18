5502ba1dd61ba83a76a76f1e100633766553da3c
DELL <DELL@DESKTOP-TT2Q8LS>
2023-04-18 09:43:44 +0530

    commiittttt

--- Framework/src/main/java/com/pages/LoginPage.java
+++ Framework/src/main/java/com/pages/LoginPage.java
@@ -8,53 +8,46 @@ import org.openqa.selenium.support.PageFactory;
 import com.utilities.WebElementUtils;
 
 public class LoginPage {
-	
+
 	WebDriver driver;
-	
-	WebElementUtils elementutil=new WebElementUtils();
-	
-	@FindBy(xpath="//input[@name='username']")
+	WebElementUtils elementutil = new WebElementUtils();
+
+	@FindBy(xpath = "//input[@name='username']")
 	WebElement username;
-	@FindBy(xpath="//input[@name='password']")
+	@FindBy(xpath = "//input[@name='password']")
 	WebElement password;
-	@FindBy(xpath="//input[@name='submit']")
+	@FindBy(xpath = "//input[@name='submit']")
 	WebElement submit;
-	
-	public  LoginPage(WebDriver driver)
-	{
-		this.driver=driver;
-		PageFactory.initElements(driver,this);
-	}
-	
-	public void enterValuetoUsername(String usernm)
-	{
-		
-		elementutil.enteringValuetoElements(driver,username,usernm);
+
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 
-	public void enterValuetoPassword(String pwsd)
-	{
-		elementutil.enteringValuetoElements(driver,password,pwsd);
+	public void enterValuetoUsername(String usernm) {
+
+		elementutil.enteringValuetoElements(driver, username, usernm);
 	}
-	
-	public void clickLoginButton()
-	{
-		elementutil.clickonTheElement(driver,submit);
+
+	public void enterValuetoPassword(String pwsd) {
+		elementutil.enteringValuetoElements(driver, password, pwsd);
 	}
-	
-	public void performlogin(String usernames,String passwords) {
+
+	public void clickLoginButton() {
+		elementutil.clickonTheElement(driver, submit);
+	}
+
+	public void performlogin(String usernames, String passwords) {
 		enterValuetoUsername(usernames);
 		enterValuetoPassword(passwords);
 		clickLoginButton();
 	}
-	
-	public HomePage login(String usernames,String passwords)
-	{
-	enterValuetoUsername(usernames);
-	enterValuetoPassword(passwords);
-	clickLoginButton();
-	return new HomePage(driver);
+
+	public HomePage login(String usernames, String passwords) {
+		enterValuetoUsername(usernames);
+		enterValuetoPassword(passwords);
+		clickLoginButton();
+		return new HomePage(driver);
 	}
-	
-	
+
 }
--- Framework/src/test/java/com/test/HomePageTest.java
+++ Framework/src/test/java/com/test/HomePageTest.java
@@ -1,16 +1,10 @@
 package com.test;
 
 import java.io.IOException;
-import java.util.Properties;
-
-//import org.checkerframework.checker.units.qual.h;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
 import com.base.AutomationBase;
 import com.pages.HomePage;
 import com.pages.LoginPage;
@@ -19,78 +13,67 @@ import com.utilities.PropertyUtil;
 import com.utilities.WaitUtils;
 import com.utilities.WebElementUtils;
 
-public class HomePageTest extends AutomationBase{
-	
+public class HomePageTest extends AutomationBase {
+
 	WebDriver driver;
 	LoginPage loginpg;
 	HomePage homepg;
-	BrowserUtils brwsrUtil=new BrowserUtils();
-	WebElementUtils elementutil=new WebElementUtils();
-	PropertyUtil property=new PropertyUtil();
-	WaitUtils waitutil=new WaitUtils();
-	
+	BrowserUtils brwsrUtil = new BrowserUtils();
+	WebElementUtils elementutil = new WebElementUtils();
+	PropertyUtil property = new PropertyUtil();
+	WaitUtils waitutil = new WaitUtils();
+
 	@BeforeMethod
-	public void preRun() throws IOException
-	{
-		
-       driver=getDriver();
-		
-		homepg=new HomePage(driver);
-		
-		brwsrUtil.launchUrl(driver,"https://qalegend.com/restaurant/");
-		//waitutil.implicitWait(driver,30);
-		loginpg=new LoginPage(driver);
-		homepg=loginpg.login("admin","password");
+	public void preRun() throws IOException {
+
+		driver = getDriver();
+
+		homepg = new HomePage(driver);
+
+		brwsrUtil.launchUrl(driver, "https://qalegend.com/restaurant/");
+		// waitutil.implicitWait(driver,30);
+		loginpg = new LoginPage(driver);
+		homepg = loginpg.login("admin", "password");
 	}
-	
-	
-	
-	@Test(priority=1,enabled=true)
-	public void ValidatetheMenuItemsDisplayedonTheHomePage()
-	{
-		SoftAssert soft=new SoftAssert();
-		boolean flagpos=homepg.isPosDisplayed();
-		soft.assertTrue(flagpos,"Fail: POS Link is not displayed");
-		
-		boolean flagpdt=homepg.isProductDisplayed();
-		soft.assertTrue(flagpdt,"Fail: Product link is not displayed");
-		
-		boolean flagstr=homepg.isStoreDisplayed();
-		soft.assertTrue(flagstr,"Fail: Store link is not displayed");
-		
-		boolean flagpepl=homepg.isPeopleDisplayed();
-		soft.assertTrue(flagpepl,"Fail: People link is not displayed");
-	
-		boolean flagsale=homepg.isSaleDisplayed();
-		soft.assertTrue(flagsale,"Fail: Sale link is not displayed");
-		
-		boolean flagexpns=homepg.isExpenseDisplayed();
-		soft.assertTrue(flagexpns,"Fail: Expense link is not displayed");
-		
-		boolean flagcatgry=homepg.isCategoryDisplayed();
-		soft.assertTrue(flagcatgry,"Fail: Category link is not displayed");
-		
-		boolean flagseting=homepg.isSettingDisplayed();
-		soft.assertTrue(flagseting,"Fail: Setting link is not displayed");
-		
-		boolean flagrept=homepg.isReportDisplayed();
-		soft.assertTrue(flagrept,"Fail: Report link is not displayed");
-		
-		boolean flaglang=homepg.isReportDisplayed();
-		soft.assertTrue(flaglang,"Fail: Language link is not displayed");
-				
-		boolean flaglogout=homepg.isLogoutDisplayed();
-		soft.assertTrue(flaglogout,"Fail: Logout button is not displayed");
-		
+
+	@Test(priority = 1, enabled = true)
+	public void ValidatetheMenuItemsDisplayedonTheHomePage() {
+		SoftAssert soft = new SoftAssert();
+		boolean flagpos = homepg.isPosDisplayed();
+		soft.assertTrue(flagpos, "Fail: POS Link is not displayed");
+
+		boolean flagpdt = homepg.isProductDisplayed();
+		soft.assertTrue(flagpdt, "Fail: Product link is not displayed");
+
+		boolean flagstr = homepg.isStoreDisplayed();
+		soft.assertTrue(flagstr, "Fail: Store link is not displayed");
+
+		boolean flagpepl = homepg.isPeopleDisplayed();
+		soft.assertTrue(flagpepl, "Fail: People link is not displayed");
+
+		boolean flagsale = homepg.isSaleDisplayed();
+		soft.assertTrue(flagsale, "Fail: Sale link is not displayed");
+
+		boolean flagexpns = homepg.isExpenseDisplayed();
+		soft.assertTrue(flagexpns, "Fail: Expense link is not displayed");
+
+		boolean flagcatgry = homepg.isCategoryDisplayed();
+		soft.assertTrue(flagcatgry, "Fail: Category link is not displayed");
+
+		boolean flagseting = homepg.isSettingDisplayed();
+		soft.assertTrue(flagseting, "Fail: Setting link is not displayed");
+
+		boolean flagrept = homepg.isReportDisplayed();
+		soft.assertTrue(flagrept, "Fail: Report link is not displayed");
+
+		boolean flaglang = homepg.isReportDisplayed();
+		soft.assertTrue(flaglang, "Fail: Language link is not displayed");
+
+		boolean flaglogout = homepg.isLogoutDisplayed();
+		soft.assertTrue(flaglogout, "Fail: Logout button is not displayed");
+
 		soft.assertAll();
-	
+
 	}
-	
-	
-	
-	
-	
-		
-	
 
 }
